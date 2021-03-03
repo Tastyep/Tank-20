1c52b1e11c61decd9d6598eb6319f3e8939c4cbb
Luc Sinet <luc.sinet@gmail.com>
2021-03-03 12:00:51 +0100

    Define a .clang-tidy file.
    - Apply suggested fixes.

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,12 @@ int main() {
   while (window.isOpen()) {
     // check all the window's events that were triggered since the last
     // iteration of the loop
-    sf::Event event;
+    sf::Event event{};
     while (window.pollEvent(event)) {
       // "close requested" event: we close the window
-      if (event.type == sf::Event::Closed) window.close();
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
     }
   }
 }
