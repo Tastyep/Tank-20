61268cf295c4d83b8410f9703127166950defda2
Luc Sinet <luc.sinet@gmail.com>
2021-04-01 11:57:54 +0200

    Update object initializations
      - Step the world at a fixed timestep of 60 ticks / second

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 #include "app/GameState.hpp"
 #include "domain/Action.hpp"
 #include "domain/Concept.hpp"
-#include "domain/Sprite.hpp"
 #include "domain/Timer.hpp"
 #include "domain/entity/Factory.hpp"
+#include "domain/entity/Identity.hpp"
+#include "domain/physic/ShapeConfManager.hpp"
 #include "interface/Event.hpp"
 #include "interface/Keyboard.hpp"
 #include "interface/Map.hpp"
@@ -13,11 +14,16 @@
 #include "interface/Window.hpp"
 #include "interface/controller/EventController.hpp"
 #include "interface/service/ControllerMapper.hpp"
+#include "interface/view/BoundingBox.hpp"
 #include "interface/view/Control.hpp"
 #include "interface/view/Fps.hpp"
 #include "interface/view/GameView.hpp"
 
+#include <PlayRho/Dynamics/StepConf.hpp>
+#include <memory>
 #include <spdlog/spdlog.h>
+
+#include <PlayRho/PlayRho.hpp>
 
 #include <chrono>
 #include <fstream>
@@ -28,32 +34,45 @@
 
 int main() {
   Interface::Window window(1400, 800, "My window");
-  // App::GameState                    gs{};
-  auto registry = std::make_shared<entt::registry>();
-  App::Controller::ActionController actionController(registry);
-  const auto                        stepDelta = 30;
+
+  // world definition
+  auto world = std::make_shared<playrho::d2::World>();
+
+  const auto stepDelta = 60;
   const auto timestep = std::chrono::milliseconds(1000) / stepDelta;
+  const auto stepConf = playrho::StepConf{.deltaTime = 1.f / stepDelta};
   std::chrono::nanoseconds acc{0};
   Domain::Timer            timer;
 
-  auto tileManager = std::make_shared<Interface::TileManager>();
-  Domain::Entity::Factory entFactory(registry);
-  auto                    map = std::make_shared<Interface::Map>(tileManager);
-  map->load("../asset/map.tmx");
-  entFactory.tank({32, 32});
+  // App::GameState                    gs{};
+  auto registry = std::make_shared<entt::registry>();
+  App::Controller::ActionController actionController(registry, world);
 
+  // Graphical resource loading
+  auto tileManager = std::make_shared<Interface::TileManager>();
+  auto shapeConfManager = std::make_shared<Domain::Physic::ShapeConfManager>();
+  Domain::Entity::Factory entFactory(registry, world, shapeConfManager);
+  auto                    map = std::make_shared<Interface::Map>(tileManager);
+  map->load("../asset/map.tmx", shapeConfManager);
+  entFactory.tank({1, 1});
+
+  // Controller mapping
   namespace Event = Interface::Event;
   auto controllerMapper =
       std::make_shared<Interface::Service::ControllerMapper>();
   Interface::Controller::EventController eventCtrler(controllerMapper);
 
+  // Views
   window.addView(map);
-  window.addView(
-      std::make_unique<Interface::View::GameView>(registry, tileManager));
+  window.addView(std::make_unique<Interface::View::GameView>(registry, world,
+                                                             tileManager));
   window.addView(
       std::make_unique<Interface::View::Fps>(std::chrono::milliseconds(250)));
   window.addView(std::make_unique<Interface::View::Control>(controllerMapper));
+  window.addView(
+      std::make_unique<Interface::View::BoundingBox>(registry, world));
 
+  // Game loop
   while (window.isOpen()) {
     auto        elapsed = timer.restart();
     Event::Type event{};
@@ -77,11 +96,9 @@ int main() {
         std::visit(handler, action);
       }
 
-      acc -= timestep;
+      playrho::d2::Step(*world, stepConf);
 
-      if (!window.isOpen()) {
-        return 0;
-      }
+      acc -= timestep;
     }
 
     window.render(elapsed);
